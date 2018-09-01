d031738a551a9ceede8b3ac2d588d6eee7fffd21
Sota Hatakeyama <choo.bla.rin@gmail.com>
2018-09-01 14:36:13 +0900

    say hi to regl

--- main.js
+++ main.js
@@ -1,1 +1,50 @@
-console.log('here we go');
+const sketch = document.querySelector('#sketch');
+const radius = 512;
+sketch.width = radius;
+sketch.height = radius;
+
+const regl = require('regl')(sketch);
+
+const drawTriangle = regl({
+  frag: `
+    precision mediump float;
+    uniform vec4 color;
+    void main() {
+      gl_FragColor = color;
+    }`,
+
+  vert: `
+    precision mediump float;
+    attribute vec2 position;
+    void main() {
+      gl_Position = vec4(position, 0, 1);
+    }`,
+
+  attributes: {
+    position: regl.buffer([
+      [-1, -1], [1, -1], [1, 1],
+      [1, 1], [-1, -1], [-1, 1]
+    ])
+  },
+
+  uniforms: {
+    color: regl.prop('color')
+  },
+
+  count: 6
+})
+
+regl.frame(({ time }) => {
+  regl.clear({
+    color: [0, 0, 0, 1],
+    depth: 1
+  })
+
+  const r = (Math.cos(time) + 1.0) / 2.0;
+  const g = (Math.sin(time) + 1.0) / 2.0;
+  const b = (Math.cos(time) + 1.0) / 2.0;
+
+  drawTriangle({
+    color: [r, g, b, 1]
+  });
+});
